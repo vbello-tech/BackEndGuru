eebd94600dc9fc4358ba8ffcada83353fc5c3060
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2024-03-29 01:25:39 +0100

    allowed apps

--- Core/settings/base.py
+++ Core/settings/base.py
@@ -28,7 +28,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "backendguru.up.railway.app",
+    '0.0.0.0',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
