75b9f333d2cdba0c43c98867e76b0d26a0174fc8
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2024-06-30 08:38:10 +0100

    fixed collectstatic in prod

--- Core/settings/production.py
+++ Core/settings/production.py
@@ -24,7 +24,7 @@ STORAGES = {
         "BACKEND": 'cloudinary_storage.storage.MediaCloudinaryStorage'
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage"
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
     },
 }
 
